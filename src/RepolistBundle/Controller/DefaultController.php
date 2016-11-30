08da6f33067c9083e1e8a2d916ace4198d172bf6
coralie <coralie.sylvestre@heliopsis.net>
2016-11-30 18:03:02 +0100

    Worked on github api use

--- src/RepolistBundle/Controller/DefaultController.php
+++ src/RepolistBundle/Controller/DefaultController.php
@@ -5,17 +5,21 @@ namespace RepolistBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
+//    /**
+//     * @Route("/hello/{name}")
+//     * @Template()
+//     */
+//    public function indexAction($name)
+//    {
+//        return array('name' => $name);
+//    }
+
+
+
 
     /** Action pour lister les repos dans la vue / api github */
     /**
@@ -23,7 +27,19 @@ class DefaultController extends Controller
      */
     public function repoListAction()
     {
-        return $this->render('full/repolist.html.twig');
+
+
+
+//        return $this->render('full/repolist.html.twig');
+
+
+        $token = 'a62cbd287b8cd973cfb06c501ff04d45567029df';
+
+        $repositories = json_decode(file_get_contents('https://api.github.com/search/repositories?q=user:CoralieHeliopsis' . $token ), true);
+
+        return $this->render('full/repolist.html.twig', array(
+            'repositories' => $repositories,
+        ));
 
 
     }
@@ -37,10 +53,12 @@ class DefaultController extends Controller
  *
  *
  *
+ *https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000
+
  *
  *
- *
- *
+ *https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
  *
  * var_dump(file_get_contents('https://api.github.com/users/CoralieHeliopsis/repos'));
+ * dump(file_get_contents('https://api.github.com/search/users?q=CoralieHeliopsis'));
  */
